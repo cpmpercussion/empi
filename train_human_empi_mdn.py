eb05f7a07f8638608aa8db9d83c2b036a4f54b8b
Charles Martin <cpm@charlesmartin.com.au>
2019-03-05 20:16:24 +0100

    human MDN training script

--- train_human_empi_mdn.py
+++ train_human_empi_mdn.py
@@ -1,15 +1,40 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 import random
 import numpy as np
-import pandas as pd
+import os
+import argparse
 import time
+import datetime
 
+# Hack to get openMP working annoyingly.
+import os
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+print("Script to train a predictive music interaction model for EMPI.)
+
+# Input and output to serial are bytes (0-255)
+# Output to Pd is a float (0-1)
+# parser = argparse.ArgumentParser(description='Trains a predictive music interaction model')
+# parser.add_argument('-d', '--dimension', type=int, dest='dimension', default=4,
+#                     help='The dimension of the data to model, must be >= 2.')
+# parser.add_argument('-s', '--source', dest='sourcedir', default='logs',
+#                     help='The source directory to obtain .log files')
+# parser.add_argument("--modelsize", default="s", help="The model size: xs, s, m, l, xl")
+# parser.add_argument('-e', "--earlystopping", dest='earlystopping', action="store_true", help="Use early stopping")
+# parser.add_argument('-p', "--patience", default=10, help="The number of epochs patience for early stopping.")
+# args = parser.parse_args()
+
+EARLY_STOPPING = True
+PATIENCE = 10
+MODEL_SIZE = 'xs'
+DIMENSION = 2
+DATA_LOCATION = 'datasets/empi-training-dataset-2d.npz'
+
+# Import Keras
 import empi_mdrnn
 import keras
 import keras.backend as K
 import tensorflow as tf
-import os
-
 # Set up environment.
 # Only for GPU use:
 os.environ["CUDA_VISIBLE_DEVICES"] = "0"
@@ -18,13 +43,40 @@ config.gpu_options.allow_growth = True
 sess = tf.Session(config=config)
 K.set_session(sess)
 
+# Choose model parameters.
+if MODEL_SIZE is 'xs':
+    mdrnn_units = 32
+    mdrnn_mixes = 5
+    mdrnn_layers = 2
+elif MODEL_SIZE is 's':
+    mdrnn_units = 64
+    mdrnn_mixes = 5
+    mdrnn_layers = 2
+elif MODEL_SIZE is 'm':
+    mdrnn_units = 128
+    mdrnn_mixes = 5
+    mdrnn_layers = 2
+elif MODEL_SIZE is 'l':
+    mdrnn_units = 256
+    mdrnn_mixes = 5
+    mdrnn_layers = 2
+elif MODEL_SIZE is 'xl':
+    mdrnn_units = 512
+    mdrnn_mixes = 5
+    mdrnn_layers = 3
+else:
+    mdrnn_units = 512
+    mdrnn_mixes = 5
+    mdrnn_layers = 2
+
+print("Model size:", MODEL_SIZE)
+print("Units:", mdrnn_units)
+print("Layers:", mdrnn_layers)
+print("Mixtures:", mdrnn_mixes)
 
 # Model Hyperparameters
 SEQ_LEN = 50
 SEQ_STEP = 1
-HIDDEN_UNITS = 128
-N_LAYERS = 2
-NUMBER_MIXTURES = 5
 TIME_DIST = True
 
 # Training Hyperparameters:
@@ -37,27 +89,22 @@ SEED = 2345
 random.seed(SEED)
 np.random.seed(SEED)
 
+with np.load(DATA_LOCATION) as loaded:
+    perfs = loaded['perfs']
 
-# Import Human Data CSV and create dt 
-perf_df = pd.read_csv('./data/2018-01-25T14-04-35-rnnbox.csv', header=0, index_col=0, parse_dates=['date'])
-perf_df['time'] = perf_df.index
-perf_df['seconds'] = perf_df.index
-perf_df.time = perf_df.time.diff()
-perf_df.time = perf_df.time.dt.total_seconds()
-perf_df = perf_df.dropna()
-perf_df.value = perf_df.value / 255.0
-corpus_df = pd.DataFrame({'t': perf_df.time, 'x': perf_df.value})
-corpus = np.array(corpus_df)
-print("Shape of corpus array:", corpus.shape)
-corpus_df.describe()
-perf_df.seconds = perf_df.seconds - perf_df.seconds[0]
-perf_df.seconds = perf_df.seconds.dt.total_seconds()
-perf_df.describe()
-
-slices = empi_mdrnn.slice_sequence_examples(corpus, SEQ_LEN+1, step_size=SEQ_STEP)
-
+print("Loaded perfs:", len(perfs))
+print("Num touches:", np.sum([len(l) for l in perfs]))
+corpus = perfs  # might need to do some processing here...processing
+# Restrict corpus to sequences longer than the corpus.
+corpus = [l for l in corpus if len(l) > SEQ_LEN+1]
+print("Corpus Examples:", len(corpus))
+# Prepare training data as X and Y.
+slices = []
+for seq in corpus:
+    slices += empi_mdrnn.slice_sequence_examples(seq,
+                                                 SEQ_LEN+1,
+                                                 step_size=SEQ_STEP)
 X, y = empi_mdrnn.seq_to_overlapping_format(slices)
-
 X = np.array(X) * empi_mdrnn.SCALE_FACTOR
 y = np.array(y) * empi_mdrnn.SCALE_FACTOR
 
@@ -66,20 +113,43 @@ print("X:", X.shape)
 print("y:", y.shape)
 
 # Setup Training Model
-model = empi_mdrnn.build_model(seq_len=SEQ_LEN, hidden_units=HIDDEN_UNITS, num_mixtures=NUMBER_MIXTURES, layers=2, time_dist=TIME_DIST, inference=False, compile_model=True, print_summary=True)
+model = empi_mdrnn.build_model(seq_len=SEQ_LEN,
+                               hidden_units=mdrnn_units,
+                               num_mixtures=mdrnn_mixes,
+                               layers=mdrnn_layers,
+                               out_dim=DIMENSION,
+                               time_dist=TIME_DIST,
+                               inference=False,
+                               compile_model=True,
+                               print_summary=True)
 
-# Setup callbacks
-model_path = "empi_mdrnn" + "-layers" + str(N_LAYERS) + "-units" + str(HIDDEN_UNITS) + "-mixtures" + str(NUMBER_MIXTURES) + "-scale" + str(empi_mdrnn.SCALE_FACTOR)
-filepath = model_path + "-E{epoch:02d}-VL{val_loss:.2f}.hdf5"
-checkpoint = keras.callbacks.ModelCheckpoint(filepath, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
+model_dir = "models/"
+model_name = "musicMDRNN" + "-dim" + str(DIMENSION) + "-layers" + str(mdrnn_layers) + "-units" + str(mdrnn_units) + "-mixtures" + str(mdrnn_mixes) + "-scale" + str(empi_mdrnn.SCALE_FACTOR)
+date_string = datetime.datetime.today().strftime('%Y%m%d-%H_%M_%S')
+
+filepath = model_dir + model_name + "-E{epoch:02d}-VL{val_loss:.2f}.hdf5"
+checkpoint = keras.callbacks.ModelCheckpoint(filepath,
+                                             monitor='val_loss',
+                                             verbose=1,
+                                             save_best_only=True,
+                                             mode='min')
 terminateOnNaN = keras.callbacks.TerminateOnNaN()
-tboard = keras.callbacks.TensorBoard(log_dir='./logs/'+model_path, histogram_freq=2, batch_size=32, write_graph=True, update_freq='epoch')
+early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', mode='min', verbose=1, patience=PATIENCE)
+tboard = keras.callbacks.TensorBoard(log_dir='./logs/' + date_string + model_name,
+                                     histogram_freq=2,
+                                     batch_size=32,
+                                     write_graph=True,
+                                     update_freq='epoch')
 
+callbacks = [checkpoint, terminateOnNaN, tboard]
+callbacks.append(early_stopping)
 # Train
-history = model.fit(X, y, batch_size=BATCH_SIZE, epochs=EPOCHS, validation_split=VAL_SPLIT, callbacks=[checkpoint, terminateOnNaN, tboard])
-#history = model.fit_generator(generator, steps_per_epoch=300, epochs=100, verbose=1, initial_epoch=0)
+history = model.fit(X, y, batch_size=BATCH_SIZE,
+                    epochs=EPOCHS,
+                    validation_split=VAL_SPLIT,
+                    callbacks=[checkpoint, terminateOnNaN, tboard])
 
 # Save final Model
-model.save('model_path' + '-final.hdf5')  # creates a HDF5 file of the model
+model.save_weights(model_dir + model_name + ".h5")
 
 print("Done, bye.")
