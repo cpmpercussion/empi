f415a3d939c1b90a0954cb3fbbb33ed9d1a567b8
Charles Martin <cpm@charlesmartin.com.au>
2019-03-12 17:13:20 +0100

    adding empi_1_test script

---
+++ empi_1_test.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd /home/pi/empi
+# Start Pd
+./start_pd.sh
+# Start the interface controller only.
+python3 empi_1_runloop.py -vm
+# After the RNN box controller exits, stop Pd
+pkill -u pi pd
