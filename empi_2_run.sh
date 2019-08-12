8c98441eb7ae84e0f4374fd5d7a167f6e542a04f
Charles Martin <cpm@charlesmartin.com.au>
2019-08-12 22:11:03 +1000

    updated a run script to exclude the runloop

--- empi_2_run.sh
+++ empi_2_run.sh
@@ -7,7 +7,7 @@ python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layer
 #python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layers2-units32-mixtures5-scale10-synth.h5" --modelsize xs --call --log --verbose &
 
 # Start interface
-python3 empi_2_runloop.py --screen
+#python3 empi_2_runloop.py --screen
 # After the RNN box controller exits, stop Pd
 pkill -u pi pd
 pkill -u pi python3
--- start_pd.sh
+++ start_pd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # starts Pd synthesis process
-pd -nogui synth/lever_synthesis.pd &
+pd -nogui synth/lever_synthesis_midi.pd &
