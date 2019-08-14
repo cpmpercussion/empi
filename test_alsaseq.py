0e17ec9f168a13e671b6ecd79a648a899a3f9f81
Charles Martin <cpm@charlesmartin.com.au>
2019-08-15 09:44:00 +1000

    trying out the alsa seq system

--- test_alsaseq.py
+++ test_alsaseq.py
@@ -3,6 +3,6 @@ import alsaseq
 alsaseq.client( 'EMPIMIDI', 1, 1, False )
 
 while 1:
-    if alsaseq.inputpending():
-    ev = alsaseq.input()
-    alsaseq.output( ev )
+	if alsaseq.inputpending():
+		ev = alsaseq.input()
+		alsaseq.output(ev)
