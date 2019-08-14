f9e7d4c049b1e446795ac08c73762c99e2834a3c
Charles Martin <cpm@charlesmartin.com.au>
2019-08-15 09:48:23 +1000

    testing connections

--- test_alsaseq.py
+++ test_alsaseq.py
@@ -1,6 +1,9 @@
 import alsaseq
+# http://pp.com.mx/python/alsaseq/project.html
+alsaseq.client('EMPIMIDI', 1, 1, False)
 
-alsaseq.client( 'EMPIMIDI', 1, 1, False )
+alsaseq.connectfrom(1, 128, 0)
+alsaseq.connectto(0, 128, 0)
 
 while 1:
 	if alsaseq.inputpending():
