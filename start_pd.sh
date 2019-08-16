344ba7a76030ef24d5995119baae5411f1c27f83
Charles Martin <cpm@charlesmartin.com.au>
2019-08-16 16:12:55 +1000

    cleanup startpd

--- start_pd.sh
+++ start_pd.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # starts Pd synthesis process
-pd -nogui -alsamidi -audiooutdev 1 -midiindev 20 -midioutdev 20 -noadc -open synth/lever_synthesis_midi.pd &
+#pd -nogui -alsamidi -audiooutdev 1 -midiindev 20 -midioutdev 20 -noadc -open synth/lever_synthesis_midi.pd &
 # Driver: sudo modprobe snd_seq_midi
 # view midi devices: aseqdump -l
 # view connections: aconnect -l
@@ -8,7 +8,8 @@ pd -nogui -alsamidi -audiooutdev 1 -midiindev 20 -midioutdev 20 -noadc -open syn
 # https://github.com/BlokasLabs/USBMIDI
 # pkill -u pi pd
 pd -nogui -alsamidi -audiooutdev 1 -mididev 128 -noadc -open synth/lever_synthesis_midi.pd &
-aconnect 20 128
+aconnect EMPIMIDI:1 Pure\ Data:0
+aconnect Pure\ Data:1 EMPIMIDI:0
 # https://alsa.opensrc.org/AlsaMidiOverview
 
 # python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layers2-units32-mixtures5-scale10-human.h5" --modelsize xs --call --log --verbose
@@ -17,7 +18,3 @@ aconnect 20 128
 # alsa xrun recovery apparently failed
 # pd: pcm.c:1168: snd_pcm_prepare: Assertion `pcm' failed.
 # Pd: signal 6
-
-
-# aconnect EMPIMIDI:1 Pure\ Data:0
-# aconnect Pure\ Data:1 EMPIMIDI:0
