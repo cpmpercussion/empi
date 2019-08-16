98c125bf5f9055d590b6eb8fcf9e1b15611a8dfb
Charles Martin <cpm@charlesmartin.com.au>
2019-08-16 16:54:48 +1000

    minor fix

--- empi_2_run.sh
+++ empi_2_run.sh
@@ -7,8 +7,8 @@ python3 empi_alsa_midi_interface.py &
 # wait a bit
 sleep 3
 # Start prediction server
-#python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layers2-units32-mixtures5-scale10-human.h5" --modelsize xs --call --log --verbose &
-python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layers2-units32-mixtures5-scale10-synth.h5" --modelsize xs --call --log --verbose &
+#python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layers2-units32-mixtures5-scale10-human.h5" --modelsize xs --call --log --verbose
+python3 predictive_music_model.py -d=2 --modelfile="models/musicMDRNN-dim2-layers2-units32-mixtures5-scale10-synth.h5" --modelsize xs --call --log --verbose
 
 # Start interface
 #python3 empi_2_runloop.py --screen
